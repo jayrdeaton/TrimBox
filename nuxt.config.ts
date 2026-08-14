9cf61c290f48445c36e7369fe7b888f826b8c4c4
Jay Deaton <jayrdeaton@gmail.com>
2026-08-14 11:43:11 +0700

    og.png

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,20 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1.0, viewport-fit=cover' },
         { name: 'apple-mobile-web-app-capable', content: 'yes' },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-        { name: 'apple-mobile-web-app-title', content: pkg.displayName }
+        { name: 'apple-mobile-web-app-title', content: pkg.displayName },
+        { name: 'description', content: pkg.description },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: pkg.displayName },
+        { property: 'og:title', content: pkg.displayName },
+        { property: 'og:description', content: pkg.description },
+        { property: 'og:url', content: 'https://trimbox.infinitetoken.com' },
+        { property: 'og:image', content: 'https://trimbox.infinitetoken.com/og.png' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '627' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: pkg.displayName },
+        { name: 'twitter:description', content: pkg.description },
+        { name: 'twitter:image', content: 'https://trimbox.infinitetoken.com/og.png' }
       ],
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
