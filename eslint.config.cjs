3a2b22ec4fc9876bbedeae0c04e30aed81aa90e0
Jay Deaton <jayrdeaton@gmail.com>
2026-08-27 15:21:30 +0700

    fleet update

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,54 +1,9 @@
 const { defineConfig } = require('eslint/config')
-const prettierRecommended = require('eslint-plugin-prettier/recommended')
-const simpleImportSort = require('eslint-plugin-simple-import-sort')
-const tsParser = require('@typescript-eslint/parser')
-const tsEslint = require('typescript-eslint')
-const vueParser = require('vue-eslint-parser')
-const pluginVue = require('eslint-plugin-vue')
 
 module.exports = defineConfig([
-  {
-    ignores: ['.nuxt/**', '.output/**', 'node_modules/**', 'coverage/**', 'dist/**', '**/*.mjs', '**/*.cjs']
-  },
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        extraFileExtensions: ['.vue']
-      }
-    }
-  },
-  {
-    files: ['**/*.vue'],
-    languageOptions: {
-      parser: vueParser,
-      parserOptions: {
-        parser: tsParser,
-        extraFileExtensions: ['.vue']
-      }
-    }
-  },
-  ...tsEslint.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
-  prettierRecommended,
-  {
-    plugins: {
-      'simple-import-sort': simpleImportSort
-    },
-    rules: {
-      'prettier/prettier': 'warn',
-      'simple-import-sort/imports': 'warn',
-      'simple-import-sort/exports': 'warn',
-      'no-console': 'warn',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-require-imports': 'off'
-    }
-  },
+  ...require('@infinitetoken/eslint-config/vue'),
   {
     files: ['src/pages/**/*.vue'],
-    rules: {
-      'vue/multi-word-component-names': 'off'
-    }
+    rules: { 'vue/multi-word-component-names': 'off' }
   }
 ])
--- prettier.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  jsxSingleQuote: true,
-  printWidth: 1000,
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'none'
-}
